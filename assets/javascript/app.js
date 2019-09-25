f9dcb6c94f9bc2bca5baf06b6574242d31318869
Lindsay <lindsay.lbm@gmail.com>
2019-09-25 08:37:11 -0500

    added buckets to catch quesitons not answered in if statements

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -55,127 +55,247 @@ function check() {
     var correct = 0;
     var wrong = 0;
     var notAnswered = 0;
+
+    // IF STATEMENTS ===================================================================================================
+    // question 1
     if (question1 == "Lane Cake") {
         correct++;
     }
+    else if ($("[name=question1]:checked").length) {
+        wrong++
+        console.log("checked");
+    }
     else {
-        wrong++;
-        console.log(question1)
+        notAnswered++;
+        console.log("Not Answered")
+
+        // question 2
     }
     if (question2 == "Arkansas") {
         correct++;
     }
-    else {
-        wrong++;
-        console.log(question2)
+    else if ($("[name=question2]:checked").length) {
+        wrong++
+        console.log("checked");
     }
+    else {
+        notAnswered++;
+    }
+
+    //question 3
     if (question3 == "Colorado") {
         correct++;
     }
-    else {
-        wrong++;
+    else if ($("[name=question3]:checked").length) {
+        wrong++
+        console.log("checked");
     }
+    else {
+        notAnswered++;
+    }
+
+    //question 4
     if (question4 == "Pepsi") {
         correct++;
     }
-    else {
-        wrong++;
+    else if ($("[name=question4]:checked").length) {
+        wrong++
+        console.log("checked");
     }
+    else {
+        notAnswered++;
+    }
+
+    // question 5
     if (question5 == "Watermellon") {
         correct++;
     }
-    else {
-        wrong++;
+    else if ($("[name=question5]:checked").length) {
+        wrong++
+        console.log("checked");
     }
+    else {
+        notAnswered++;
+    }
+
+    // question 6
     if (question6 == "Philly Cheesesteak") {
         correct++;
     }
-    else {
-        wrong++;
+    else if ($("[name=question6]:checked").length) {
+        wrong++
+        console.log("checked");
     }
+    else {
+        notAnswered++;
+    }
+
+    // question 7
     if (question7 == "Dried Fruit") {
         correct++;
     }
-    else {
-        wrong++;
+    else if ($("[name=question7]:checked").length) {
+        wrong++
+        console.log("checked");
     }
+    else {
+        notAnswered++;
+    }
+
+    // question 8
     if (question8 == "Deep-dish Pizza") {
         correct++;
     }
-    else {
-        wrong++;
+    else if ($("[name=question8]:checked").length) {
+        wrong++
+        console.log("checked");
     }
+    else {
+        notAnswered++;
+    }
+
+    // question 9
     if (question9 == "Ice Cream") {
         correct++;
     }
-    else {
-        wrong++;
+    else if ($("[name=question9]:checked").length) {
+        wrong++
+        console.log("checked");
     }
+    else {
+        notAnswered++;
+    }
+
+    // question 10
     if (question10 == "November 3") {
         correct++;
     }
-    else {
-        wrong++;
+    else if ($("[name=question10]:checked").length) {
+        wrong++
+        console.log("checked");
     }
+    else {
+        notAnswered++;
+    }
+
+    // question 11
     if (question11 == "Bread") {
         correct++;
     }
-    else {
-        wrong++;
+    else if ($("[name=question11]:checked").length) {
+        wrong++
+        console.log("checked");
     }
+    else {
+        notAnswered++;
+    }
+
+    // question 12
     if (question12 == "Fried Chicken") {
         correct++;
     }
-    else {
-        wrong++;
+    else if ($("[name=question12]:checked").length) {
+        wrong++
+        console.log("checked");
     }
+    else {
+        notAnswered++;
+    }
+
+    // question 13
     if (question13 == "Georgia") {
         correct++;
     }
-    else {
-        wrong++;
+    else if ($("[name=question13]:checked").length) {
+        wrong++
+        console.log("checked");
     }
+    else {
+        notAnswered++;
+    }
+
+    // question 14
     if (question14 == "Turkey") {
         correct++;
     }
-    else {
-        wrong++;
+    else if ($("[name=question14]:checked").length) {
+        wrong++
+        console.log("checked");
     }
+    else {
+        notAnswered++;
+    }
+
+    // question 15
     if (question15 == "Key Lime Pie") {
         correct++;
     }
-    else {
-        wrong++;
+    else if ($("[name=question15]:checked").length) {
+        wrong++
+        console.log("checked");
     }
+    else {
+        notAnswered++;
+    }
+
+    // question 16
     if (question16 == "Ketchup") {
         correct++;
     }
-    else {
-        wrong++;
+    else if ($("[name=question16]:checked").length) {
+        wrong++
+        console.log("checked");
     }
+    else {
+        notAnswered++;
+    }
+
+    // question 17
     if (question17 == "Ginger Ale") {
         correct++;
     }
-    else {
-        wrong++;
+    else if ($("[name=question17]:checked").length) {
+        wrong++
+        console.log("checked");
     }
+    else {
+        notAnswered++;
+    }
+
+    // question 18
     if (question18 == "Chocolate Milk") {
         correct++;
     }
-    else {
-        wrong++;
+    else if ($("[name=question18]:checked").length) {
+        wrong++
+        console.log("checked");
     }
+    else {
+        notAnswered++;
+    }
+
+    // question 19
     if (question19 == "Smores") {
         correct++;
     }
-    else {
-        wrong++;
+    else if ($("[name=question19]:checked").length) {
+        wrong++
+        console.log("checked");
     }
+    else {
+        notAnswered++;
+    }
+
+    // question 20
     if (question20 == "Cherries") {
         correct++;
     }
+    else if ($("[name=question20]:checked").length) {
+        wrong++
+        console.log("checked");
+    }
     else {
-        wrong++;
+        notAnswered++;
     }
     document.getElementById("results").style.visibility = "visible";
     document.getElementById("numCorrect").innerHTML = "Correct: " + correct;
