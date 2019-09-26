bc29730fa498d7e70745eb5afe441a0e5a0cb2af
Lindsay <lindsay.lbm@gmail.com>
2019-09-25 19:28:21 -0500

    moved var questions back into function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,26 +5,6 @@
 
 // GLOBAL VARIABLES================================================================================================
 var time = 120;
-var question1 = document.quiz.question1.value;
-var question2 = document.quiz.question2.value;
-var question3 = document.quiz.question3.value;
-var question4 = document.quiz.question4.value;
-var question5 = document.quiz.question5.value;
-var question6 = document.quiz.question6.value;
-var question7 = document.quiz.question7.value;
-var question8 = document.quiz.question8.value;
-var question9 = document.quiz.question9.value;
-var question10 = document.quiz.question10.value;
-var question11 = document.quiz.question11.value;
-var question12 = document.quiz.question12.value;
-var question13 = document.quiz.question13.value;
-var question14 = document.quiz.question14.value;
-var question15 = document.quiz.question15.value;
-var question16 = document.quiz.question16.value;
-var question17 = document.quiz.question17.value;
-var question18 = document.quiz.question18.value;
-var question19 = document.quiz.question19.value;
-var question20 = document.quiz.question20.value;
 
 
 // FUNCTIONS=======================================================================================================
@@ -45,6 +25,26 @@ $(".start").click(function start() {
 
 //function to check the score
 function check() {
+    var question1 = document.quiz.question1.value;
+    var question2 = document.quiz.question2.value;
+    var question3 = document.quiz.question3.value;
+    var question4 = document.quiz.question4.value;
+    var question5 = document.quiz.question5.value;
+    var question6 = document.quiz.question6.value;
+    var question7 = document.quiz.question7.value;
+    var question8 = document.quiz.question8.value;
+    var question9 = document.quiz.question9.value;
+    var question10 = document.quiz.question10.value;
+    var question11 = document.quiz.question11.value;
+    var question12 = document.quiz.question12.value;
+    var question13 = document.quiz.question13.value;
+    var question14 = document.quiz.question14.value;
+    var question15 = document.quiz.question15.value;
+    var question16 = document.quiz.question16.value;
+    var question17 = document.quiz.question17.value;
+    var question18 = document.quiz.question18.value;
+    var question19 = document.quiz.question19.value;
+    var question20 = document.quiz.question20.value;
 
     var correct = 0;
     var wrong = 0;
